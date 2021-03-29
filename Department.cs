66d90d079197de7907f1692d177dd2532558c4b4
abhinay <63472859+abhinay-developer@users.noreply.github.com>
2021-03-29 10:29:39 +0530

    Add files via upload

---
+++ Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+
+    public class Department
+    {
+        public int departmentId { get; set; }
+        public string  departmentName { get; set; }
+    }
+}
